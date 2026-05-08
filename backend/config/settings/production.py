25f01a56de603c3f30f702b1af1b1813ace578f6
Balaji <135409419+balaji2k423@users.noreply.github.com>
2026-05-08 10:51:35 +0530

    fix production settings and secret key

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,8 +1,16 @@
 from .base import *
+import os
 
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv(
+    'CSRF_TRUSTED_ORIGINS',
+    'https://dmw-customer-portal-3.onrender.com'
+).split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
